322bd8ddca528585aae2cb9dee83ba2ceb8b63a6
Sagnik <sdey@iastate.edu>
2024-11-07 13:00:07 -0600

    firing

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,6 +14,7 @@ public class Main {
             System.out.println("3. Checkout");
             System.out.println("4. Exit");
             System.out.println("5. Firing");
+            System.out.println("6. Hiring");
             System.out.print("Choose an option: ");
             int choice = scanner.nextInt();
             scanner.nextLine(); // Consume newline left-over
@@ -64,28 +64,42 @@ public class Main {
                     // Counts how many times the same violation has occurred
                     int violationCount = 0;
 
+
+
                     Hiring h = new Hiring("TX69", "Molly", "Cashier", true, 13);
-                    Hiring h2 = new Hiring("TX69", "Molly", "Cashier", true, 13);
+
                     Firing f = new Firing("UC27", "Leo", true, false, false);
-                    Firing f2 = new Firing("UC27", "Leo", true, false, false);
 
                     HashMap<Integer, String> policies = new HashMap<>();
 
+
+                    ArrayList<String> names = new ArrayList<>();
+
+                    names.add("Lexi");
+                    names.add("Richard");
+                    names.add("Lauren");
+
+
+
                     // Insert policies into a HashMap
                     policies.put(1, "absent for work more than 3 times without manager approval");
-                    policies.put(2, "discrimination on the basis of religion, race, linguistics, beliefs, culture or sex");
-                    policies.put(3, "verbal harassment and/or violence");
-                    policies.put(4, "sexual assault");
-                    policies.put(5, "time fraud");
+                    policies.put(2, "verbal harassment and/or violence");
+                    policies.put(3, "time fraud");
+                    policies.put(4, "Racism");
+
 
                     String filePath = "C:\\grocery\\362-grocery\\testFile.txt";
 
+
                     try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
                         String line;
+                        int i =0;
                         while ((line = br.readLine()) != null) {
 
                             if (!line.isEmpty()) {
                                 int firstChar = Character.getNumericValue(line.charAt(0));
+
+
 
                                 // Check if the line is in the HashMap as a key
                                 if (policies.containsKey(firstChar)) {
@@ -93,20 +107,60 @@ public class Main {
                                     violationCount++;
                                     if (violationCount == 1) {
                                         f.warning = true;
-                                        System.out.println("WARNING: " + f.employeeName);
-                                    } else if (violationCount == 2) {
+                                        System.out.println("WARNING: " + names.get(i));
+                                    } if (violationCount == 2) {
                                         f.meetingWithManager = true;
-                                        System.out.println("Schedule a meeting with the manager: " + f.employeeName);
-                                    } else if (violationCount == 3) {
+                                        System.out.println("Schedule a meeting with the manager: " + names.get(i));
+                                    }  if (violationCount == 3) {
                                         f.isFired = true;
-                                        System.out.println("Fired: " + f.employeeName);
+                                        System.out.println("Fired: " + names.get(i));
                                     }
                                 } else {
                                     System.out.println("Not found in hashmap: " + line);
                                 }
                             }
-                        }
+
+                            i++;}
                     } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 6:
+                    HashMap<Integer, String> requirements = new HashMap<>();
+
+                    requirements.put(1, "full-time student status");
+                    requirements.put(2, "no criminal charges");
+
+                    ArrayList<String> empNames = new ArrayList<>();
+
+                    // Add names to the ArrayList
+                    empNames.add("Leo");
+
+                    empNames.add("Emma");
+
+                    String filePath2 = "C:\\grocery\\362-grocery\\testFile2.txt";
+
+                    try (BufferedReader br = new BufferedReader(new FileReader(filePath2))) {
+                        String line;
+                        int i =0;
+                        while ((line = br.readLine()) != null) {
+
+
+                            if (!line.isEmpty()) {
+                                int firstChar = Character.getNumericValue(line.charAt(0));
+
+                                // Check if the line is in the HashMap as a key
+                                if (requirements.containsKey(firstChar)) {
+                                    //System.out.println("Found in hashmap: " + line);
+                                    System.out.println(empNames.get(i));
+                                    }
+                                } else {
+                                    System.out.println("Not found in hashmap: " + line);
+                                }
+                            i++;}
+
+                            }
+                     catch (IOException e) {
                         e.printStackTrace();
                     }
                     break;
