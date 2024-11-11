772001c2bad892b0285f2385cba36ba89e7a3d2b
Gabriel Unser <gcunser@gmail.com>
2024-11-10 21:41:32 -0600

    something approaching a basic implementation

--- SalesRecord.java
+++ SalesRecord.java
@@ -1,3 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Gabriel_Unser
+ */
 public class SalesRecord {
+    public static void viewSalesRecords() {
+        try {
+            File file = new File("salesRecords.txt");
+
+            if (file.exists()) {
+                java.util.Scanner scanner = new java.util.Scanner(file);
+                while (scanner.hasNextLine()) {
+                    System.out.println(scanner.nextLine()); // Print each line of the sales records
+                }
+                scanner.close();
+            } else {
+                System.out.println("Sales records not found.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading sales records: " + e.getMessage());
+        }
+    }
 
 }
